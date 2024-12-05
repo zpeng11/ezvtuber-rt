30e31727a2e6be7a2acb38bb0b47bc7dd5cea69a
zpeng11 <a2843173630@gmail.com>
2024-12-04 22:11:38 -0800

    introduce turbojpeg for caching

--- ezvtb_rt/cache.py
+++ ezvtb_rt/cache.py
@@ -12,55 +12,44 @@ import os
 from multiprocessing import Process, Queue, shared_memory
 import multiprocessing as mp
 import queue
+import turbojpeg
 
 class Cacher(ABC):
     def read(hs:int) -> np.ndarray:
         pass
     def write(hs:int, data:np.ndarray):
         pass
-
-def THARes2JPEG(res:np.ndarray, jpeg_quality:int = 90) -> bytes:
-    saveInt = ((res / 2.0 + 0.5) * 255.0).clip(0.0, 255.0).astype(np.uint8)
-    shapes = res.shape
-    reshaped = saveInt.reshape(shapes[1],shapes[2] * shapes[3]).transpose().reshape(shapes[2], shapes[3], shapes[1])
-    cvimg = cv2.cvtColor(reshaped, cv2.COLOR_RGB2BGR)
-    if jpeg_quality < 100:
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
-        ret = zlib.compress(cv2.imencode('.jpg', cvimg, encode_param)[1].tobytes())
-    else:
-        ret = zlib.compress(cv2.imencode('.png', cvimg)[1].tobytes())
-    return ret
-
-
-def JPEG2THARes(data:bytes , dtype:np.dtype = np.float16)->np.ndarray: 
-    cvimg = cv2.imdecode(np.fromstring(zlib.decompress(data), dtype="uint8") , cv2.IMREAD_COLOR)
-    cvimg = cv2.cvtColor(cvimg, cv2.COLOR_BGR2RGB)
-    shapes = cvimg.shape
-    reshaped = cvimg.reshape(shapes[0] * shapes[1], shapes[2]).transpose().reshape(1, shapes[2], shapes[0], shapes[1])
-    totype = reshaped.astype(dtype)
-    return (totype / 255.0 - 0.5) * 2.0
     
 
 class RAMCacher(Cacher):
-    def __init__(self, max_size:int, dtype:np.dtype = np.float16, quality:int = 90): #Size in GBs
+    def __init__(self, max_size:int, dtype:np.dtype = np.float16, quality:int = 90, alpha:bool = True): #Size in GBs
         self.max_size = max_size
         self.cache = OrderedDict()
         self.hits = 0
         self.miss = 0
         self.dtype = dtype
         self.quality = quality
+        self.alpha = alpha
     def read(self, hs:int) -> np.ndarray:
         cached = self.cache.get(hs)
         if cached is not None:
             self.hits += 1
             self.cache.move_to_end(hs)
-            img = JPEG2THARes(cached, self.dtype)
+            if self.alpha:
+                res = turbojpeg.decompress(cached, fastdct = True, fastupsample=True, pixelformat=turbojpeg.BGRA)
+                img = np.ndarray((512,512,4), dtype=np.uint8, buffer=res)
+            else:
+                res = turbojpeg.decompress(cached, fastdct = True, fastupsample=True, pixelformat=turbojpeg.BGR)
+                img = np.ndarray((512,512,3), dtype=np.uint8, buffer=res)
             return img
         else:
             self.miss += 1
             return None
     def write(self, hs:int, data:np.ndarray) -> tuple[int, np.ndarray]:
-        self.cache[hs] = THARes2JPEG(data, self.quality)
+        if self.alpha:
+            self.cache[hs] = turbojpeg.compress(data, self.quality, turbojpeg.SAMP.Y420,fastdct = True, optimize= True, pixelformat=turbojpeg.BGRA)
+        else:
+            self.cache[hs] = turbojpeg.compress(data, self.quality, turbojpeg.SAMP.Y420,fastdct = True, optimize= True, pixelformat=turbojpeg.BGR)
         if len(self.cache) * len(self.cache[hs]) > self.max_size * 1024 * 1024 * 1024:
             return self.cache.popitem(last=False)
 
@@ -70,12 +59,10 @@ class ReaderProcess(Process): # A seperate process that reads input from databas
     def __init__(self, read_trigger:Queue, 
                  read_return:Queue, 
                  db_path:str, max_size:int, #DB max ram size
-                 dtype:np.dtype = np.float16
                  ):
         super().__init__()
         self.read_trigger = read_trigger
         self.read_return = read_return
-        self.dtype = dtype
         self.db_path = db_path
         self.max_size = max_size
 
@@ -94,8 +81,7 @@ class ReaderProcess(Process): # A seperate process that reads input from databas
             if not ret:
                 self.read_return.put_nowait((hs, None))
             else:
-                img = JPEG2THARes(ret[1], self.dtype)
-                self.read_return.put_nowait((hs, img))
+                self.read_return.put_nowait((hs, ret[1]))
         self.conn.close()
 
     def create_db_for_read(self, db_path:str, max_size:int):
@@ -128,8 +114,7 @@ class WriterProcess(Process): # A seperate process that write input from databas
             if res is None or res is not tuple:
                 break
             hs = res[0]
-            data = THARes2JPEG(res[1])
-            self.conn.execute('INSERT OR IGNORE INTO cache(hash, bytes) VALUES (?, ?)',(hs, data))
+            self.conn.execute('INSERT OR IGNORE INTO cache(hash, bytes) VALUES (?, ?)',(hs, res[1]))
             self.conn.commit()
         self.conn.close()
 
@@ -145,17 +130,19 @@ class WriterProcess(Process): # A seperate process that write input from databas
 
 
 class DBCacherMP(Cacher):
-    def __init__(self, max_size:int = 1,  db_dir:str = '.', dtype:np.dtype = np.float16) -> None:
+    def __init__(self, max_size:int = 1,  db_dir:str = '.', cache_quality:int = 90, alpha:bool = True) -> None:
         os.makedirs(db_dir, exist_ok=True)
         db_path = os.path.join(db_dir, 'cacher.sqlite')
+        self.cache_quality = cache_quality
+        self.alpha = alpha
         self.read_trigger = Queue()
         self.read_return = Queue()
-        self.reader = ReaderProcess(self.read_trigger, self.read_return, db_path, max_size, dtype)
+        self.reader = ReaderProcess(self.read_trigger, self.read_return, db_path, max_size)
         self.reader.start()
         ready = self.read_return.get(timeout=1.0)
         assert(ready in 'ready')
         self.write_queue = Queue()
-        self.writer = WriterProcess(self.write_queue, db_path, max_size, max_size)
+        self.writer = WriterProcess(self.write_queue, db_path, max_size)
         self.writer.start()
         self.hits = 0
         self.miss = 0
@@ -164,19 +151,31 @@ class DBCacherMP(Cacher):
     def read(self, hs:int) -> np.ndarray:
         self.read_trigger.put_nowait(hs)
         try:
-            ret = self.read_return.get(block=True, timeout=0.01) #Only wait for 10ms for read here
+            ret = self.read_return.get(block=True, timeout=0.001) #Only wait for 1ms for read here
             while(ret[0] is int and ret[0] != hs):
-                ret = self.read_return.get(block=True, timeout=0.01) # This works for removing missed read
+                ret = self.read_return.get(block=True, timeout=0.001) # This works for removing missed read
             if ret[1] is None:
                 self.miss += 1
+                return None
             else:
                 self.hits += 1
-            return ret[1]
+                if self.alpha:
+                    buf = turbojpeg.decompress(ret[1], fastdct = True, fastupsample=True, pixelformat=turbojpeg.BGRA)
+                    return np.ndarray((512,512,4), dtype=np.uint8, buffer=buf)
+                else:
+                    buf = turbojpeg.decompress(ret[1], fastdct = True, fastupsample=True, pixelformat=turbojpeg.BGR)
+                    return np.ndarray((512,512,3), dtype=np.uint8, buffer=buf)
+
         except queue.Empty:
+            print('Warning DB read slow!')
             return None
 
     def write(self, hs:int, data:np.ndarray):
-        self.write_queue.put_nowait((hs, data))
+        if self.alpha:
+            cache_bytes = turbojpeg.compress(data, self.cache_quality, turbojpeg.SAMP.Y420,fastdct = True, optimize= True, pixelformat=turbojpeg.BGRA)
+        else:
+            cache_bytes = turbojpeg.compress(data, self.cache_quality, turbojpeg.SAMP.Y420,fastdct = True, optimize= True, pixelformat=turbojpeg.BGR)
+        self.write_queue.put_nowait((hs, cache_bytes))
 
     def close(self):
         self.read_trigger.put_nowait(None)
